ae6b780e82fd04301fe371a607fa7d460d580709
salemebo <salem.ebo@gmail.com>
2022-08-13 22:22:45 +0200

    add app root component

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,1 +1,15 @@
-export const test = 'test';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Component } from '@ibyar/aurora';
+
+import '@ibyar/bs-button';
+
+
+@Component({
+	selector: 'app-root',
+	template: `<bs-btn-toggle class="btn btn-outline-primary">Test Bootstrap Toggle Button</bs-btn-toggle>`,
+})
+export class AppRootComponent {
+
+}
