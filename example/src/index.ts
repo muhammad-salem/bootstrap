90494d2f30fa8a7457bc3df92df761682526931e
salemebo <salem.ebo@gmail.com>
2022-08-13 23:30:33 +0200

    add btns

--- example/src/index.ts
+++ example/src/index.ts
@@ -8,7 +8,19 @@ import '@ibyar/bs-button';
 
 @Component({
 	selector: 'app-root',
-	template: `<bs-btn-toggle class="btn btn-outline-primary">Test Bootstrap Toggle Button</bs-btn-toggle>`,
+	template: `
+	<bs-btn-toggle class="btn btn-outline-primary">Test Bootstrap Toggle Button</bs-btn-toggle>
+
+	<bs-btn-outline-primary>bs-btn-outline-primary</bs-btn-outline-primary>
+	<bs-btn-outline-secondary>bs-btn-outline-secondary</bs-btn-outline-secondary>
+	<bs-btn-outline-success>bs-btn-outline-success</bs-btn-outline-success>
+	<bs-btn-outline-danger>bs-btn-outline-danger</bs-btn-outline-danger>
+	<bs-btn-outline-warning>bs-btn-outline-warning</bs-btn-outline-warning>
+	<bs-btn-outline-info>bs-btn-outline-info</bs-btn-outline-info>
+	<bs-btn-outline-light>bs-btn-outline-light</bs-btn-outline-light>
+	<bs-btn-outline-dark>bs-btn-outline-dark</bs-btn-outline-dark>
+	
+	`,
 })
 export class AppRootComponent {
 
